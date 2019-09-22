d5ff3a53fb788edb9488cb9bb42e52379c7ed2e8
Gustavo Borges Coutinho <gustavoborgescoutinho@MacBook-Air-de-Gustavo.local>
2019-09-21 22:45:43 -0300

    Versao inicial

---
+++ src/main/java/com/kbal/ProjectKabalApplication.java
@@ -0,0 +1,13 @@
+package com.kbal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKabalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectKabalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbal/ProjectKabalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kbal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectKabalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
